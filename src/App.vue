92c6fb01d53e0a3f55d697332d7322c1a92a9776
Santiago Ariel Villanueva <santiagovillanuevaariel@gmail.com>
2023-02-27 21:15:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Copyright from "./components/Copyright.vue";
+
+Copyright;
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Copyright />
+</template>
